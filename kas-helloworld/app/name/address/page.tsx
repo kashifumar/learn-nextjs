df4120cd8bf7f78df93f677da170667615775682
Kashif Umar <Kashif.TLB@gmail.com>
2023-01-03 15:35:06 +0500

    First Commit

---
+++ kas-helloworld/app/name/address/page.tsx
@@ -0,0 +1,5 @@
+export default function GetAddress(){
+  return (
+    <h2>I Live In Lahore</h2>
+  )
+}
---
+++ kas-helloworld/app/name/page.tsx
@@ -0,0 +1,5 @@
+export default function GetName(){
+  return (
+    <h1>My Name Is Kashif Umar</h1>
+  )
+}
